a184527a17adba70f212deb417eeee02362b2a4d
Stephen Anderson <s.anderson@kainos.com>
2016-06-14 09:18:02 +0100

    list of skateboards is working

---
+++ WebFrontEnd/Controllers/BoardsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccess;
+using WebFrontEnd.Models.Board;
+
+namespace WebFrontEnd.Controllers
+{
+    public class BoardsController : Controller
+    {
+        // GET: Boards
+        public ActionResult Index()
+        {
+            var model = new List<BoardModel>();
+            using (var db = new lookatmyskateboardEntities())
+            {
+                model.AddRange(db.Skateboards.Select(skateboard => new BoardModel
+                {
+                    Id = skateboard.id, Name = skateboard.name, ImageUrl = skateboard.imageUrl, Description = skateboard.description, UploadedBy = skateboard.User.username
+                }));
+            }
+
+            return View(model);
+        }
+
+        public ActionResult View()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
