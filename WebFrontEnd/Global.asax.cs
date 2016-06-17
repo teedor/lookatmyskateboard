6d6e6c336aee71d8750851b131f1a4a0cf92f519
Stephen Anderson <s.anderson@kainos.com>
2016-06-17 15:18:13 +0100

    session cookie can be stolen

--- WebFrontEnd/Global.asax.cs
+++ WebFrontEnd/Global.asax.cs
@@ -17,5 +17,19 @@ namespace WebFrontEnd
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        void Application_EndRequest(Object sender, EventArgs e)
+        {
+            if (Response.Cookies.Count > 0)
+            {
+                foreach (string s in Response.Cookies.AllKeys)
+                {
+                    if (s == "ASP.NET_SessionId")
+                    {
+                        Response.Cookies["ASP.NET_SessionId"].HttpOnly = false;
+                    }
+                }
+            }
+        }
     }
 }
